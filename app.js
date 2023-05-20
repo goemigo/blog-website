21fd2f65058ffdc9231fe2c29647bb331e9caf14
Shige Wang <113735355+goemigo@users.noreply.github.com>
2023-05-19 23:10:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -127,7 +127,7 @@ app.post("/signup",function(req,res){
 app.post("/failure",function(req,res){
   res.redirect("/");
 })
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Server running on port ${port}`);
 });
